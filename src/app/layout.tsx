a6665f8ac8de8b6e07b65f1b26cc633b2bfd8ccf
Cornerstone <fortunecornerstone@gmail.com>
2026-03-06 00:43:10 +0100

    update font import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,20 @@
 import type { Metadata } from "next";
+import { Bebas_Neue, DM_Sans } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
+
+const bebasNeue = Bebas_Neue({
+  variable: "--font-bebas-neue",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Project Relentless",
@@ -38,7 +51,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${bebasNeue.variable} ${dmSans.variable}`}>
       <body>
         <Navbar />
         <main>{children}</main>
